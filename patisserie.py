31dd33e331c184b02771d3d65ad8d30c5acdff54
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-10 16:03:33 +0200

    ajout main et correction fondeur et batteur avec recepient a la place de quantité et nbs_ouefs

--- patisserie.py
+++ patisserie.py
@@ -35,22 +35,23 @@ class Chocolat(Ingredient):
 
 
 class BatteurOeufs(Commis):
-    def __init__(self, nom, nb_oeufs):
+    def __init__(self, nom, recipient):
         super().__init__(nom)
-        self.nb_oeufs = nb_oeufs
+        self.recipient = recipient
 
     def run(self):
         # on suppose qu'il faut 8 tours de batteur par œuf présent dans le bol
-        nb_tours = self.nb_oeufs * 8
+        oeuf = self.recipient.contenu
+        nb_tours = oeuf.quantite * 8
         for no_tour in range(1, nb_tours + 1):
-            print(f"\tJe bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
+            print(f"\tJe bats les {oeuf.quantite} oeufs, tour n°{no_tour}")
             time.sleep(0.5)  # temps supposé d'un tour de batteur
 
 
 class FondeurChocolat(Commis):
-    def __init__(self, nom, quantite):
+    def __init__(self, nom, recipient):
         super().__init__(nom)
-        self.quantite = quantite  # en grammes
+        self.recipient = recipient  # en grammes
 
     def run(self):
         print("Je mets de l'eau à chauffer dans une bouilloire")
@@ -61,9 +62,10 @@ class FondeurChocolat(Commis):
         time.sleep(1)
         # on suppose qu'il faut 1 tour de spatule par 10 g. de chocolat
         # présent dans le bol pour faire fondre le chocolat
-        nb_tours = math.ceil(self.quantite / 10)
+        chocolat = self.recipient.contenu
+        nb_tours = math.ceil(chocolat.quantite / 10)
         for no_tour in range(1, nb_tours + 1):
-            print(f"Je mélange {self.quantite} de chocolat à fondre, tour n°{no_tour}")
+            print(f"Je mélange {chocolat.quantite} de chocolat à fondre, tour n°{no_tour}")
             time.sleep(1)  # temps supposé d'un tour de spatule
 
 class Appareil:
@@ -89,12 +91,21 @@ class Recipient:
     def __str__(self):
         return self.nom
 
+def main():
+    cul_de_poule = Recipient("le cul-de-poule", Oeuf(6))
+    bol_chocolat_1 = Recipient("le bol de chocolat n°1", Chocolat(100))
+    bol_chocolat_2 = Recipient("le bol de chocolat n°2", Chocolat(100))
+
+    batteur = BatteurOeufs("Alice, commis batteur", cul_de_poule)
+    fondeur_1 = FondeurChocolat("Basile, commis fondeur", bol_chocolat_1)
+    fondeur_2 = FondeurChocolat("Chloé, commis fondeur", bol_chocolat_2)
+
+    batteur.start()
+    fondeur_1.start()
+    fondeur_2.start()
+    batteur.join()
+    fondeur_1.join()
+    fondeur_2.join()
 
 if __name__ == "__main__":
-    batteur = BatteurOeufs("alfred", 6)
-    fondeur = FondeurChocolat("victor",200)
-    batteur.start()
-    fondeur.start()
-    batteur.join()
-    fondeur.join()
-    print("\nJe peux à présent incorporer le chocolat aux oeufs")
+    main()
